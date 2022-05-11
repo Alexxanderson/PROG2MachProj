4c5795c973bdb74ce39689b7a1e7a14f80b9e941
Alexxanderson <bien_aaron_miranda@dlsu.edu.ph>
2022-05-12 07:11:39 +0800

    Moved the functions below, initialized string.h, changed pokemon var to 999

--- MainProject.c
+++ MainProject.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-
-void clrscr() // Clear Screen
-{
-    system("@cls||clear");
-}
+#include <string.h>
 
 struct pokemon
 {
@@ -11,29 +7,11 @@ struct pokemon
     char cPokeName[21];
     char cPokeType;
     char cPokeDesc[51];
-}pokemon[100];
+}pokemon[999];
 
-void addEntry(int i)
-{
-        pokemon[i].entry = i + 1;
-         printf ("For Entry no. %d, \n", pokemon[i].entry);
-         printf ("Enter pokemon name: ");
-         fgets(pokemon[i].cPokeName, 21, stdin);
-         printf ("Is the pokemon [W]Water, [F]Fire, [G]Grass, [E]Electric: ");
-         scanf(" %c", &pokemon[i].cPokeType);
-         printf("Description: ");
-         fgets(pokemon[i].cPokeDesc,51, stdin);
-         fgets(pokemon[i].cPokeDesc,51, stdin);
-         printf("\n");
-}
-
-void displayEntries(int i)
-{
-        printf("Entry No. %d\n", pokemon[i].entry);
-        printf("Pokemon Name: %s\n", pokemon[i].cPokeName);
-        printf("Pokemon Type: %c\n", pokemon[i].cPokeType);
-        printf("Description: %s\n", pokemon[i].cPokeDesc);
-}
+void addEntry(int i);
+void clrscr(); // Clear Screen}
+void displayEntries(int i);
 
 //main
 int main()
@@ -52,4 +30,31 @@ int main()
     }
 
     return 0;
+}
+
+void addEntry(int i)
+{
+        pokemon[i].entry = i + 1;
+         printf ("For Entry no. %d, \n", pokemon[i].entry);
+         printf ("Enter pokemon name: ");
+         fgets(pokemon[i].cPokeName, 21, stdin);
+         printf ("Is the pokemon [W]Water, [F]Fire, [G]Grass, [E]Electric: ");
+         scanf(" %c", &pokemon[i].cPokeType);
+         printf("Description: ");
+         fgets(pokemon[i].cPokeDesc,51, stdin);
+         fgets(pokemon[i].cPokeDesc,51, stdin);
+         printf("\n");
+}
+
+void clrscr() // Clear Screen
+{
+    system("@cls||clear");
+}
+
+void displayEntries(int i)
+{
+        printf("Entry No. %d\n", pokemon[i].entry);
+        printf("Pokemon Name: %s\n", pokemon[i].cPokeName);
+        printf("Pokemon Type: %c\n", pokemon[i].cPokeType);
+        printf("Description: %s\n", pokemon[i].cPokeDesc);
 }
