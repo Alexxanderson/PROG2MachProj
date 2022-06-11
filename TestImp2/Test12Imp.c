a3e5e131b4d7c064b1bba20d807578800b98c829
Alexxanderson <bien_aaron_miranda@dlsu.edu.ph>
2022-06-11 17:35:58 +0800

    test things

--- TestImp2/Test12Imp.c
+++ TestImp2/Test12Imp.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+
+struct pokemonstruct
+{
+    int entry;
+    char cPokeName[21];
+    char cPokeType;
+    char cPokeDesc[51];
+    int nResearchType[2];
+};
 
 void clrscr() // Clear Screen
 {
@@ -9,29 +20,46 @@ void clrscr() // Clear Screen
 int main()
 {
     clrscr();
-    char inName[20];
-    char ctype = 'c';
-    char cDescp[21];
+    struct pokemonstruct pokemon[99];
+    char inName[99];
+    char ctype[99];
+    char cDescp[99];
 
 
-    printf("Enter a name: ");
-    scanf(" %[^\n]s", &inName); //this is for asking the name of the file you want to get
-    printf("Enter a description: ");
-    scanf(" %[^\n]s", &cDescp); //this is for asking the name of the file you want to get
+    // printf("Enter a name: ");
+    // scanf(" %[^\n]s", &inName); //this is for asking the name of the file you want to get
+    // printf("Enter a description: ");
+    // scanf(" %[^\n]s", &cDescp); //this is for asking the name of the file you want to get
 
     FILE *fpointer;
+    FILE *fpointsss;
 
-    fpointer = fopen("trystring.txt","w"); // w = write, r = read, a = append
+    fpointsss = fopen("try3.txt","r"); // w = write, r = read, a = append ; for opening the file
 
-       fprintf(fpointer, "The string is: %s \n", inName);
-       fprintf(fpointer, "The type is: %c \n", ctype);
-       fprintf(fpointer, "The description is: %s \n", cDescp);
-       
-    
 
-    fclose(fpointer);
+        fscanf(fpointsss, " %[^\n]s", &inName); // trying to scan everything, automatically na magiiscan to the next line to sabi daw
+        fscanf(fpointsss, " %[^\n]s", &ctype);
+         fscanf(fpointsss, " %[^\n]s", &cDescp);
+
+    fclose(fpointsss); // file closing
+
+    printf(" %s\n", inName);
+    printf(" %s\n", ctype);
+    printf(" %s\n", cDescp);
+
 
     printf("Hello world \n");
     return 0;
 }
 // setMain < INPUT.txt > MIRANDA-ACTUAL.txt
+
+
+    // fpointer = fopen("trystring.txt","w"); // w = write, r = read, a = append
+
+    //    fprintf(fpointer, "The string is: %s \n", inName);
+    //    fprintf(fpointer, "The type is: %c \n", ctype);
+    //    fprintf(fpointer, "The description is: %s \n", cDescp);
+       
+    
+
+    // fclose(fpointer);
---
+++ TestImp2/Test13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void clrscr() // Clear Screen
+{
+    system("@cls||clear");
+}
+
+int main()
+{
+    clrscr();
+    printf("Hello world \n");
+    return 0;
+}
+// setMain < INPUT.txt > MIRANDA-ACTUAL.txt
